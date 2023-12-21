9aa9f9a2eb6f3fbe3976c9b393ec610179e52091
faisal sha <faisal.sha@niveussolutions.com>
2023-12-21 17:44:10 +0530

    model addded for adding product

---
+++ lib/Models/productModel.dart
@@ -0,0 +1,13 @@
+class ProductModel {
+  String? productName;
+  String? productImage;
+  String? productPrice;
+  String? productDescription;
+
+  ProductModel({
+    required this.productDescription,
+    required this.productImage,
+    required this.productName,
+    required this.productPrice,
+  });
+}
